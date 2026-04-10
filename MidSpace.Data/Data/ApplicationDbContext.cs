92b3fc6022a1161cf6bf6be60a89e0eede7460c6
AhmedAbdghaniey <168416270+AhmedAbdghaniey@users.noreply.github.com>
2026-04-10 17:45:24 +0200

    Add files via upload

--- MidSpace.Data/Data/ApplicationDbContext.cs
+++ MidSpace.Data/Data/ApplicationDbContext.cs
@@ -11,13 +11,13 @@ namespace MidSpace.Data.Data
     internal class ApplicationDbContext : DbContext
     {
         DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; } 
-        public DbSet<Doctor> Doctors { get; set; } 
-        public DbSet<Patient> Patients { get; set; } 
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
         public DbSet<DoctorCertificate> DoctorCertificates { get; set; }
-        public DbSet<DoctorAvailability> DoctorAvailabilities { get; set; } 
-        public DbSet<PatientAllergy> PatientAllergies { get; set; } 
-        public DbSet<PatientChronicDisease> PatientChronicDiseases { get; set; } 
+        public DbSet<DoctorAvailability> DoctorAvailabilities { get; set; }
+        public DbSet<PatientAllergy> PatientAllergies { get; set; }
+        public DbSet<PatientChronicDisease> PatientChronicDiseases { get; set; }
         public DbSet<UserPhone> UserPhones { get; set; }
         public DbSet<Admin> Admins { get; set; }
         DbSet<Subscription> Subscriptions { get; set; }
@@ -25,11 +25,28 @@ namespace MidSpace.Data.Data
         DbSet<PlanFeature> PlanFeatures { get; set; }
         DbSet<Payment> Payments { get; set; }
         DbSet<Appointment> Appointments { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Data Source=db47147.public.databaseasp.net;Initial Catalog=db47147;User Id=db47147;Password=7Qq_N@4o!eF2;Encrypt=False;");
-        }
-        
 
+       // Chat
+
+
+public DbSet<Chat> Chats { get; set; }
+    public DbSet<ChatParticipant> ChatParticipants { get; set; }
+    public DbSet<Message> Messages { get; set; }
+
+    //Notifications
+public DbSet<Notification> Notifications { get; set; }
+
+    //Community
+public DbSet<Post> Posts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<EducationalContent> EducationalContents { get; set; }
+    public DbSet<DoctorFollow> DoctorFollows { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=db47147.public.databaseasp.net;Initial Catalog=db47147;User Id=db47147;Password=7Qq_N@4o!eF2;Encrypt=False;");
     }
+
+
+}
 }
